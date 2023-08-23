03782929675569e6d692dc8816a4862431a5519f
Marco Andre Schmidt <mandree29@outlook.com>
2023-08-23 19:26:41 -0300

    treinando com canvas

---
+++ canvas.js
@@ -0,0 +1,66 @@
+import { gerar_posX, gerar_poxY, tamanho_raio, velocidade } from "./circulo.js"
+
+var canvas = document.querySelector('canvas')
+var c = canvas.getContext('2d')
+
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+
+const mathpi = Math.PI * 2 
+var bolas_arrary = []
+
+
+function circulo(x,y, r, vw, vh){
+   this.x = x // x é a posição de nascimento vertical do circulo
+   this.y = y // y é a posição de nascimento horizontal do circulo
+   this.vw = vw // velocidade do circulo
+   this.vh = vh
+   this.r = r// tamanho do raio 
+
+   this.desenho = function(){
+       c.beginPath()
+       c.arc(this.x, this.y, this.r, 0, mathpi, false)
+       c.strokeStyle = 'orange'
+       c.stroke()
+   }
+
+   this.atualizacao = function(){
+      this.x = this.x + this.vw
+      this.y = this.y + this.vh
+
+      if(this.x + this.r >= innerWidth || this.x - this.r < 0){
+         this.vw = this.vw * -1
+      }
+      if(this.y + this.r >= innerHeight || this.y - this.r < 0){
+         this.vh = this.vh * -1 
+      }
+
+      this.desenho()
+   }
+}
+
+
+for (let index = 0; index < 200; index++) {
+   bolas_arrary.push(new circulo(gerar_posX(), gerar_poxY(), tamanho_raio(), velocidade(), velocidade()))  
+}
+for (let index = 0; index < bolas_arrary.length; index++) {
+   bolas_arrary[index].desenho()
+   
+}
+
+
+function animation(){
+     requestAnimationFrame(animation)
+     c.clearRect(0,0, innerWidth, innerHeight)
+     for (let index = 0; index < bolas_arrary.length; index++) {
+         bolas_arrary[index].atualizacao()
+      
+     }
+}
+animation()
+
+
+
+
+
---
+++ circulo.js
@@ -0,0 +1,54 @@
+
+export function gerar_posX(){
+    let x = parseInt(Math.random() * innerWidth)
+    if (x < 30){
+        x = x + 30
+    }
+    if (x > 1400 ){
+        x = x - 30
+    }
+    return x
+} 
+
+
+export function gerar_poxY(){
+    let y = parseInt(Math.random() * innerHeight)
+    if (y < 30){
+        y = y + 30
+    }
+    if (y > 970){
+        y = y - 30
+    }
+    return y
+}
+
+export function tamanho_raio(){
+    let r = parseInt(Math.random() * 30)
+    if (r == 1){
+        r += 1
+    }
+    return r
+}
+
+function gerador_velocidade(){
+    let v = parseInt(Math.random() * 10)
+    if(v == 0){
+        v = 1
+    }
+    return v
+}
+function negativo_ou_positivo(){
+    let n = parseInt(Math.random() * 2)
+    return n
+}
+
+
+export function velocidade(){
+  let v = gerador_velocidade()
+  let sorteio = negativo_ou_positivo()
+  if(sorteio > 0){
+    return v
+  }else{
+    return -v
+  }
+}
